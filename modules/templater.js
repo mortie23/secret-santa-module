6802353d4a2e516f55e5bd97fb50d843036036d4
Christopher Mortimer <christopher@mortimer.xyz>
2020-11-28 21:20:59 +1100

    update template

--- modules/templater.js
+++ modules/templater.js
@@ -1,19 +1,20 @@
 'use strict';
 
-var blueTemplate = require('blueimp-tmpl');
-var DEFAULT_TEMPLATE = '<div>You should prepeare for <h3>{%=o.to%}</h3></div>';
+const blueTemplate = require('blueimp-tmpl');
+
+var DEFAULT_TEMPLATE = '<div>Your sectret Santa for 2020 is <h3>{%=o.to%}</h3></div>';
 
 module.exports = function templater(options, array) {
-    var simpleTemplateFunction = blueTemplate(options.template ? options.template : DEFAULT_TEMPLATE);
+  var simpleTemplateFunction = blueTemplate(options.template ? options.template : DEFAULT_TEMPLATE);
 
-    var processArrayToTemplates = array
-        .map(function(object) {
-            return { giver: object[0], to: object[1] };
-        }).map(function(object) {
-            return { giver: object.giver, string: simpleTemplateFunction(object) };
-        });
-
-    return new Promise(function(resolve, reject) {
-        Promise.all(processArrayToTemplates).then(resolve).catch(reject);
+  var processArrayToTemplates = array
+    .map((object) => {
+      return { giver: object[0], to: object[1] };
+    }).map((object) => {
+      return { giver: object.giver, string: simpleTemplateFunction(object) };
     });
+
+  return new Promise((resolve, reject) => {
+    Promise.all(processArrayToTemplates).then(resolve).catch(reject);
+  });
 };
