b615d4e4a8b85f425624c4b22ac94f7f3fbee519
Christopher Mortimer <christopher@mortimer.xyz>
2020-11-28 21:20:12 +1100

    formatting

--- modules/logger.js
+++ modules/logger.js
@@ -7,21 +7,21 @@ var pathExists = require('path-exists');
 var LOG_DIR = './logs';
 
 if (!pathExists.sync(LOG_DIR)) {
-    fs.mkdirSync(LOG_DIR);
+  fs.mkdirSync(LOG_DIR);
 }
 
 var logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.File)({ filename: LOG_DIR + '/app.log' })
-    ]
+  transports: [
+    new (winston.transports.File)({ filename: LOG_DIR + '/app.log' })
+  ]
 });
 
 logger.add(winston.transports.Console, {
-    prettyPrint: true,
-    colorize: true,
-    silent: false,
-    timestamp: false,
-    level: (process.env.NODE_ENV !== 'production' ? 'verbose' : 'error')
+  prettyPrint: true,
+  colorize: true,
+  silent: false,
+  timestamp: false,
+  level: (process.env.NODE_ENV !== 'production' ? 'verbose' : 'error')
 });
 
 module.exports = logger;
