3c60ad47a97f32d1f884571a9dc12001e870cca6
Christopher Mortimer <christopher@mortimer.xyz>
2020-11-28 21:19:42 +1100

    pass whole object randomizeArray

--- service.js
+++ service.js
@@ -1,35 +1,31 @@
 'use strict';
 
-var neatCsv = require('neat-csv');
-var mail = require('./modules/mail');
-var logger = require('./modules/logger');
-var templater = require('./modules/templater');
-var randomizeArray = require('secret-santa-shuffler');
+const neatCsv = require('neat-csv');
+const mail = require('./modules/mail');
+const logger = require('./modules/logger');
+const templater = require('./modules/templater');
+const randomizeArray = require('./modules/shuffler');
 
-var prepeareForTemplater = function(originalList, newList) {
-    return newList.map(function(item) {
-        var santeeMail = item[1];
-        var santee = originalList.find(function(singleSantee) {
-            return singleSantee.email === santeeMail;
-        });
-        return [item[0], santee.name + ' ' + '(' + santeeMail + ')'];
+const prepeareForTemplater = (originalList, newList) => {
+  return newList.map((item) => {
+    var santeeMail = item[1];
+    var santee = originalList.find((singleSantee) => {
+      return singleSantee.email === santeeMail;
     });
+    return [item[0], santee.name + ' ' + '(' + santeeMail + ')'];
+  });
 };
 
-module.exports = function(list, options) {
-    return new Promise(function(resolve, reject) {
-        neatCsv(list, function(error, mailingObject) {
-            if (error) {
-                reject(error);
-                return;
-            }
-            var mailList = mailingObject.map(function(singleObject) {
-                return singleObject.email;
-            });
-
-            var prepearedtForTemplater = prepeareForTemplater(mailingObject, randomizeArray(mailList));
-            var compiledMail = options.debug ? resolve : mail(options);
-            templater(options, prepearedtForTemplater).then(compiledMail).then(resolve).catch(logger.error);
-        });
+module.exports = function (list, options) {
+  return new Promise((resolve, reject) => {
+    neatCsv(list, (error, mailingObject) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      var prepearedtForTemplater = prepeareForTemplater(mailingObject, randomizeArray(mailingObject));
+      var compiledMail = options.debug ? resolve : mail(options);
+      templater(options, prepearedtForTemplater).then(compiledMail).then(resolve).catch(logger.error);
     });
+  });
 };
